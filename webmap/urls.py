a7d7ca2cc853e78bd172ee1d707a25906f54a270
Jacobgijah <jacobgijjah@gmail.com>
2024-07-08 13:33:21 +0300

    v2 updates init

--- webmap/urls.py
+++ webmap/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+admin.site.site_header = 'Drone nofly zone'
+admin.site.site_title = 'Admin'
+admin.site.index_title = 'Admin panel'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('map.urls')),
